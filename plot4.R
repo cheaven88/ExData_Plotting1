fd6d914604cae84f9fa5d6f0f09bd408b3cd4c25
Chelsea Heaven <cheaven88@gmail.com>
2014-06-05 22:19:36 -0400

    Added graphs and R code

---
+++ plot4.R
@@ -0,0 +1,10 @@
+#Working drive
+setwd("C:/Users/Mike/Documents/Online Courses/Coursera/Exploratory Data Analysis/Course Project 1")
+getwd()
+
+#importing data
+dat.import <- read.table("household_power_consumption.txt", sep = ";", na.strings = c("NA", "?"), header = TRUE, nrows=2075259)
+dat <- subset(dat.import, Date %in% c("1/2/2007", "2/2/2007"))
+
+#creating the plot
+plot(dat$Date, dat$Global_active_power)
